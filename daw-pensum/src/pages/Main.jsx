732748b5543f9a903916d7b87de648c443b5ddeb
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-10-07 22:00:56 -0600

    .

--- daw-pensum/src/pages/Main.jsx
+++ daw-pensum/src/pages/Main.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
+//Imagenes
+import BannerMain from "/src/assets/img/bannerMain.jpg";
+
 export default function Main() {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <div
+      className="hero h-[30rem]"
+      style={{
+        backgroundImage: `url(${BannerMain})`, // Establece la URL de la imagen de fondo
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md text-white">
+          <h1 className="mb-5 text-5xl font-bold">Universidad Don Bosco</h1>
+          <p className="mb-5 text-xl">La Universidad de las Generaciones</p>
         </div>
       </div>
     </div>
